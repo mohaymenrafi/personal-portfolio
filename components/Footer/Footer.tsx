32ab502f7b693fecf0f84fecf00ca5fd6ded3872
mohaymenrafi <mohaymen.rafi@gmail.com>
2022-06-16 11:47:30 +0600

    footer added

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-white font-mono text-13 text-center pt-12 pb-8">
+      <p>
+        Made with 🤍 by <span className="text-green">MhAbdullah</span>
+      </p>
+      <p>
+        Thanks <a href="https://brittanychiang.com/">Brittany</a> for the
+        awesome design
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import About from "../components/About/About";
 import Banner from "../components/Banner/Banner";
 import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Projects from "../components/Projects/Projects";
 
@@ -21,6 +22,7 @@ const Home: NextPage = () => {
           <About />
           <Projects />
           <Contact />
+          <Footer />
         </div>
       </main>
     </div>
