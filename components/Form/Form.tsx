bd1190f03a56c0a5213b2f34fa0b8d93c266dc62
mohaymenrafi <mohaymen.rafi@gmail.com>
2022-06-19 13:09:48 +0600

    contact form connected with email js

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
 interface Inputs {
   name: string;
@@ -13,18 +14,34 @@ const Form = () => {
     handleSubmit,
     watch,
     register,
+    reset,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const [success, setSuccess] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-  //   console.log(watch("example"));
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    emailjs
+      .send(
+        "service_ksp4bds",
+        "template_v543g8a",
+        data,
+        "user_sWZHIrLmexbYpoTT6zgpN"
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          setSuccess("Your form is sent successfully");
+        }
+      })
+      .catch((err) => {
+        setError("Error occured! Please submit your form again.");
+      });
+    reset();
+  };
 
   return (
     <div className="max-w-2xl mx-auto">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col space-y-6"
-      >
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col ">
         <input
           placeholder="Full Name"
           type="text"
@@ -33,10 +50,13 @@ const Form = () => {
         />
         <input
           placeholder="Your Email"
-          type="text"
-          {...register("email")}
+          type="email"
+          {...register("email", { required: true })}
           className="input-one"
         />
+        {errors.email?.type === "required" && (
+          <p className="text-red-500 mt-2">Email is required</p>
+        )}
         <input
           placeholder="Subject"
           type="text"
@@ -46,15 +66,15 @@ const Form = () => {
         <textarea
           rows={4}
           placeholder="Your message"
-          {...register("message")}
+          {...register("message", { required: true })}
           className="input-one"
         />
-        {/* <input defaultValue={"test"} {...register("example")} />
-        <input {...register("exampleRequired", { required: true })} />
-        {errors.exampleRequired && <span>This field is required</span>} */}
+        {errors.message?.type === "required" && (
+          <p className="text-red-500 mt-2">Message is required</p>
+        )}
         <input
           type="submit"
-          className="text-green border border-slate hover:border-green hover:bg-green py-3 px-16 text-[14px] font-mono cursor-pointer hover:text-navy self-start transition-all"
+          className="text-green border border-slate hover:border-green hover:bg-green py-3 px-16 mt-6 text-[14px] font-mono cursor-pointer hover:text-navy self-start transition-all"
         />
       </form>
     </div>
