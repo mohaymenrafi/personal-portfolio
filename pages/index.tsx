3194f9ca8a2561d39091204b4bdc6274d6b4b2bc
mohaymen_rafi <mohaymen.rafi@gmail.com>
2022-06-20 23:55:25 +0600

    Project details page added (#3)
    * real project data added
    
    * project image style changed in  mobile
    
    * projects space fixed
    
    * project details page added
    
    * render project details fixed
    
    * Link changed to a tag and open link in a new tab
    
    * project feature updaed for coffee store
    
    * banner text container size fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Head from "next/head";
 import About from "../components/About/About";
 import Banner from "../components/Banner/Banner";
 import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
-import Header from "../components/Header/Header";
 import LeftSidebar from "../components/LeftSidebar/LeftSidebar";
 import Projects from "../components/Projects/Projects";
 import RightSidebar from "../components/RightSidebar/RightSidebar";
@@ -17,19 +15,17 @@ const Home: NextPage = () => {
         <meta name="description" content="I am a frontend developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+
       <main className="bg-navy px-12 md:px-100 lg:px-150 relative">
         <div className="lg:max-w-5xl mx-auto">
           <Banner />
           <About />
           <Projects />
           <Contact />
-
           <LeftSidebar />
           <RightSidebar />
         </div>
       </main>
-      <Footer />
     </div>
   );
 };
